b00135c0d38b59fc7a02044770302dca36bdf486
Brayan Barroso <brayanbarroso01@gmail.com>
2023-01-17 14:45:21 -0500

    actualizando js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,32 @@
 const notificationAlert = document.querySelector(".notification-alert");
 const notificationButton = document.querySelector(".notification-button");
 const notificationText = document.querySelectorAll(".notification-text");
-const active = document.querySelectorAll(".active");
+const textNotification = document.querySelectorAll(".active");
 
-let number = active.length;
+let number = textNotification.length;
 
-notificationAlert.innerHTML = number;
+if (number > 0) {
+    notificationAlert.innerHTML = number;
+    textNotification.forEach(obj => obj.addEventListener("click",()=>{
+        obj.classList.remove("active");
+        number--;
+            // console.log(number)
+        notificationAlert.innerHTML = number;
+        if (number == 0) {
+            // console.log("soy cero "+number);
+            number++;
+    
+        }
+
+        
+    }))
+    
+    
+}
+
+function deleteNotification(){
+    textNotification.forEach(element => element.classList.remove('active'));
+    notificationAlert.innerHTML = 0;
+}
+
+notificationButton.addEventListener("click",deleteNotification);
